8b2da8ccab3067cda8dc88ddc31b1fcc4f15d1f3
“saschakala” <mxsaschakala@gmail.com>
2020-12-10 18:27:54 -0500

    migrated database and successfully added seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_215442) do
+ActiveRecord::Schema.define(version: 2020_12_10_230231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.integer "score"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2020_12_10_215442) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "games", "users"
 end
