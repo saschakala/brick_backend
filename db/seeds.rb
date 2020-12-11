ce0081c71cef9d38c5c026f96e2e41002f3e1a69
“saschakala” <mxsaschakala@gmail.com>
2020-12-10 20:52:25 -0500

    added namespaced route games index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Game.destroy_all
+
 5.times do
     User.create(name: Faker::Name.name_with_middle)
 end
