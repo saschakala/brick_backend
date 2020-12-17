c6ead8ab0dd22dfcde830c4f27752e887f213e22
Sascha Kala <mxsaschakala@gmail.com>
2020-12-16 22:09:26 -0500

    debugged user_name catch error

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
+  validate :user_exists
   validates :score, presence: true
 
   def user_name=(name)
@@ -8,6 +9,12 @@ class Game < ApplicationRecord
 
   def user_name
     self.user ? self.user.name : :nil
+  end
+
+  def user_exists
+    if self.user.name == "" || !self.user.name
+      self.errors.add :no_name, "must provide a name"
+    end
   end
 
 end
